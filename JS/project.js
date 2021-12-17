8788ef93c08f3374cfe5e7a1155ca3dca633dc6b
Rebecca <njokinyagah@gmail.com>
2021-12-17 15:55:50 +0300

    first commit

--- JS/project.js
+++ JS/project.js
@@ -1,7 +1,8 @@
 const akanNameCalculator = (CC, YY, MM, DD, gender) => {
-  
+   
     const calculationResult = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
     let wholeNumber = Number(calculationResult.toFixed())
+    console.log(wholeNumber);
 
     const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
@@ -19,7 +20,6 @@ const akanNameCalculator = (CC, YY, MM, DD, gender) => {
     }
 }
 
-
 const handleClick = () => {
    
     const birthDayBox = document.getElementById("birthDay")
@@ -27,6 +27,7 @@ const handleClick = () => {
     const birthYearBox = document.getElementById("birthYear")
     const birthGenderBox = document.getElementById("birthGender")
 
+  
     const birthDay = birthDayBox.value;
     const birthMonth = birthMonthBox.value;
     const birthYear = birthYearBox.value;
@@ -35,16 +36,16 @@ const handleClick = () => {
    
     const allGendersArray = ["Male", "Female"]
     if (allGendersArray.includes(birthGender)) {
-       
+     
         const centuryVar = String(birthYear).substring(0, 2)
         const yearVar = String(birthYear).substring(2)
-        
+          
         if (birthDay <= 0 || birthDay > 31) {
             alert('Please enter a valid Day')
         } else if (birthMonth <= 0 || birthMonth > 12) {
             alert('Please enter a valid month')
         } else {
-         
+       
             const akanName = akanNameCalculator(centuryVar, yearVar, birthMonth, birthDay, birthGender);
             alert(`Your Akan name is ${akanName.name} born on ${akanName.day}`)
         }
